7816667e2f46f1d5abae04c9afba8377089fe605
s-tiger <tjdghek98@naver.com>
2020-11-26 15:54:06 +0900

    validation 적용

--- src/main/java/study/shop/domain/member/MemberReqDTO.java
+++ src/main/java/study/shop/domain/member/MemberReqDTO.java
@@ -5,17 +5,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 @NoArgsConstructor //기본 생성자
 @AllArgsConstructor //모든 변수 생성자
 public class MemberReqDTO {
 
     @ApiModelProperty(value = "로그인ID", required = true)
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
     private String loginId;
+    //@NotBlank : null과 빈 공백 문자열 (" ")을 허용하지 않음
+    //@NotEmpty : null과 공백 문자열("")을 허용하지 않음
 
     @ApiModelProperty(value = "패스워드", required = true)
+    @NotBlank(message = "패스워드는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{8,20}"/*(?=.*\W)*/,
+            message = "비밀번호는 영문자, 특수문자가 1개 이상 들어간 8~20자의 비밀번호여야 합니다.")
     private String password;
-
+    //@Pattern : 정규표현식에 맞는 문자열이어야 함 
+    // (?=.*[0-9]) 숫자가 적어도 하나
+    // (?=.*[a-zA-Z]) 영문 대,소문자중 적어도 하나
+    // (?=.*\W) 특수문자 적어도 하나
+    // (?=\S+$) 공백 제거
+    // .{8,20} 8글자 ~ 20글자 사이
+    
     @ApiModelProperty(value = "닉네임", required = true)
     private String nickName;
 
